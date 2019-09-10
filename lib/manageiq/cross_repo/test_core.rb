bfbbd6647fc776d631900e45421d91cfdc5780d2
Adam Grare <agrare@redhat.com>
2019-09-10 11:42:41 -0400

    Raise an exception if no gems were overridden

--- lib/manageiq/cross_repo/test_core.rb
+++ lib/manageiq/cross_repo/test_core.rb
@@ -3,6 +3,8 @@ module ManageIQ::CrossRepo
     attr_reader :core_repo, :gem_repos
 
     def initialize(core_repo, gem_repos)
+      raise ArgumentError, "You must pass at least one gem to override" if gem_repos.to_a.empty?
+
       @core_repo = Repository.new(core_repo)
       @gem_repos = gem_repos.to_a.map { |repo| Repository.new(repo) }
     end
@@ -17,7 +19,6 @@ module ManageIQ::CrossRepo
       Dir.chdir(core_repo.path) do
         require_relative core_repo.path.join("lib", "manageiq", "environment").to_s
         ManageIQ::Environment.create_database_user if ENV["CI"]
-        require "bundler"
         Bundler.with_clean_env do
           system!("bin/setup")
           system!("bundle exec rake")
