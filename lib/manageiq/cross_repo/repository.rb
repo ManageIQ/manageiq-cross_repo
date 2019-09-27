53df31f8267f64c7e792a31a327f588ec7abb876
Adam Grare <agrare@redhat.com>
2019-09-27 12:59:12 -0400

    Fix content_dir extraction when using Pathname
    Pathname returns the full path so we were trying to move
    /tmp/dir/tmp/dir/manageiq-repo

--- lib/manageiq/cross_repo/repository.rb
+++ lib/manageiq/cross_repo/repository.rb
@@ -29,7 +29,7 @@ module ManageIQ::CrossRepo
       Dir.mktmpdir do |dir|
         Minitar.unpack(Zlib::GzipReader.new(open(tarball_url, "rb")), dir)
 
-        content_dir = File.join(dir, Pathname.new(dir).children.detect(&:directory?))
+        content_dir = Pathname.new(dir).children.detect(&:directory?)
         FileUtils.mkdir_p(path.dirname)
         FileUtils.mv(content_dir, path)
       end
