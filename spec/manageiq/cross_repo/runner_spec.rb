50e8539cd9831cf5f1164912365af48491091297
Adam Grare <agrare@redhat.com>
2021-01-26 21:18:19 -0500

    Add specs for #build_test_script

--- spec/manageiq/cross_repo/runner_spec.rb
+++ spec/manageiq/cross_repo/runner_spec.rb
@@ -32,4 +32,60 @@ describe ManageIQ::CrossRepo::Runner do
       expect(runner.script_cmd).to eq("cat db/schema.rb")
     end
   end
+
+  describe "#build_test_script (private)" do
+    let(:runner) do
+      described_class.new(:test_repo => nil, :repos => []).tap do |r|
+        require "yaml"
+        allow(r).to receive(:travis_yml).and_return(YAML.load(travis_yml))
+      end
+    end
+
+    let(:travis_yml) do
+      <<~TRAVIS_YML
+        ---
+        language: ruby
+        cache: bundler
+        script: bundle exec rake
+      TRAVIS_YML
+    end
+
+    it "builds a test script" do
+      expected_test_script = <<~SCRIPT
+        #!/bin/bash
+
+        echo 'travis_fold:start:script'
+        bundle exec rake || exit $?
+        echo 'travis_fold:end:script'
+      SCRIPT
+
+      expect(runner.send(:build_test_script)).to eq(expected_test_script)
+    end
+
+    context "with arrays of commands" do
+      let (:travis_yml) do
+        <<~TRAVIS_YML
+          ---
+          language: ruby
+          cache: bundler
+          script:
+          - bundle exec rake
+          - bundle exec rake spec:javascript
+        TRAVIS_YML
+      end
+
+      it "builds a test script with both commands" do
+        expected_test_script = <<~SCRIPT
+          #!/bin/bash
+
+          echo 'travis_fold:start:script'
+          bundle exec rake || exit $?
+          bundle exec rake spec:javascript || exit $?
+          echo 'travis_fold:end:script'
+        SCRIPT
+
+        expect(runner.send(:build_test_script)).to eq(expected_test_script)
+      end
+    end
+  end
 end
